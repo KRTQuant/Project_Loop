e5e80937e9d7a729bd5d82a099cf91d0f447c2c7
KRTQuant <supanat.ubon@outlook.com>
2023-08-04 11:50:04 +0700

    feat: add zoom by right click

--- Assets/Script/PlayerCamera.cs
+++ Assets/Script/PlayerCamera.cs
@@ -6,6 +6,8 @@ public class PlayerCamera : MonoBehaviour
 {
     [Header("Camera control")]
     [SerializeField]
+    private Camera camera;
+    [SerializeField]
     private float senseX;
     [SerializeField]
     private float senseY;
@@ -18,6 +20,24 @@ public class PlayerCamera : MonoBehaviour
     [SerializeField]
     private Transform playerCamHolder;
 
+    [Header("Camera FOV")]
+    [SerializeField]
+    private float normalFov;
+    [SerializeField]
+    private float zoomFov;
+
+    [Header("Lerp")]
+    [SerializeField]
+    private float beginValue;
+    [SerializeField]
+    private float endValue;
+    [SerializeField]
+    private float lerpDuration;
+    [SerializeField]
+    private float lerpElapedTime;
+    [SerializeField]
+    private bool isLerp;
+
     private float rotationX;
     private float rotationY;
 
@@ -29,7 +49,37 @@ public class PlayerCamera : MonoBehaviour
 
     void Update()
     {
+        this.UpdateCameraRotation();
+        this.SyncCameraPos();
 
+        this.ComputeInput();
+        this.LerpFOV();
+    }
+
+    private void ComputeInput()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            this.isLerp = true;
+            if(this.camera.fieldOfView == zoomFov)
+            {
+                this.UnzoomCamera();
+            }
+
+            else
+            {
+                this.ZoomCamera();
+            }
+        }
+    }
+
+    private void SyncCameraPos()
+    {
+        this.cameraTransform.position = this.playerCamHolder.position;
+    }
+
+    private void UpdateCameraRotation()
+    {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * this.senseX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * this.senseY;
 
@@ -40,12 +90,35 @@ public class PlayerCamera : MonoBehaviour
 
         this.transform.rotation = Quaternion.Euler(this.rotationX, this.rotationY, 0);
         this.orientation.rotation = Quaternion.Euler(0, this.rotationY, 0);
-
-        this.SyncCameraPos();
     }
 
-    private void SyncCameraPos()
+    private void ZoomCamera()
     {
-        this.cameraTransform.position = this.playerCamHolder.position;
+        this.beginValue = this.normalFov;
+        this.endValue = this.zoomFov;
+        this.lerpElapedTime = 0;
+    }
+
+    private void UnzoomCamera()
+    {
+        this.beginValue = this.zoomFov;
+        this.endValue = this.normalFov;
+        this.lerpElapedTime = 0;
+    }
+
+    private void LerpFOV()
+    {
+        if(!this.isLerp) return;
+
+        var fov = this.camera.fieldOfView;
+        
+        this.lerpElapedTime += Time.deltaTime;
+        var percentage = this.lerpElapedTime / this.lerpDuration;
+        
+        this.camera.fieldOfView = Mathf.Lerp(this.beginValue, this.endValue, percentage);
+        if(percentage >= 1)
+        {
+            this.isLerp = false;
+        }
     }
 }
